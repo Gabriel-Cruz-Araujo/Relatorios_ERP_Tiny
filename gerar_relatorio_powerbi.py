916edf7ba4357ce0a591a0c8b320a4d235a9e9c3
Gabriel <aracajuextratosdaterra@gmail.com>
2025-09-19 14:49:40 -0300

    refactor:♻️ mudando para pegar a data do dia anterior.

--- gerar_relatorio_powerbi.py
+++ gerar_relatorio_powerbi.py
@@ -1,10 +1,14 @@
 from src.relatorios.relatorio_pedidos_de_venda_diario import relatorio_pdv_diario
+from datetime import datetime, timedelta
 from datetime import datetime
 """
 Rotina que alimenta o 
-painel do power bi
+painel do power bi e os caixas diarios
 
 """
-data = datetime.now().strftime("%d-%m-%Y")
 
-relatorio_pdv_diario(data)
+ontem = datetime.now() - timedelta(days=1)
+data_ontem = ontem.strftime("%d-%m-%Y")
+
+
+relatorio_pdv_diario(data_ontem)
